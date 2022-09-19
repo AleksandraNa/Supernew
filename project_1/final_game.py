ccd863da5226c1ee6e7dd5ca28ce0f7b466e8bd2
Aleksandra <umnik72@bk.ru>
2022-09-19 14:27:23 +0500

    final_game.ipyb

--- project_1/final_game.py
+++ project_1/final_game.py
@@ -1,19 +1,32 @@
-import numbers
+"""Игра - угадай число. Компьютер сам загадывает и сам угадывает 
+рандомное число за минимальное количество попыток."""
+
 import random
-from re import T
 
-number = random.randint(1, 101)
-count = 0
-min1 = 1
-max1 = 100
+number = random.randint(1, 100) # загадываем число в пределах от 1 до 100
+count = 0 # количество попыток, за которое компьютер угадает число
+min1 = 1 # минимально возможное число
+max1 = 101 # максимально возможное число
+average_count = 0 # среднее число попыток угадывания за 1 цикл
+number_of_counts = 0 # количество циклов
 
-while True:
-    count+=1
-    mid = (min1 + max1) // 2
-    if mid > number:
-        max1 = mid
-    elif mid < number:
-        min1 = mid
-    elif mid == number:
-        print(f"Поздравляем, загаданное число {number}, за {count} попыток")
-        break
+while number_of_counts < 1000:
+    count += 1
+    mid = (min1 + max1) // 2 # компьютер пробует угадать среднее число
+    if mid > number: # если загаданное число меньше,
+        max1 = mid # среднее число становится максимальным
+    elif mid < number: # если загаданное число больше,
+        min1 = mid # среднее становится минимальным
+    else:
+        number_of_counts += 1 
+        average_count = average_count + count # складываем общее количество попыток
+        number = random.randint(1, 100) # выбираем новое число для угадывания
+        count = 0
+        min1 = 1
+        max1 = 101
+
+average_count = average_count // 1000 # среднее число попыток угадывания за 1 цикл
+print(f"Выполнено 1000 раз, среднее число попыток угадывания {average_count}")
+
+if __name__ == '__main__':
+    average_count
